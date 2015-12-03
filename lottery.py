78546f1d8ee18cc31c26774f6bd61c89be1293d9
woodman <ykyx00@gmail.com>
2015-12-03 16:10:16 +0800

    修复bug若干

--- lottery.py
+++ lottery.py
@@ -2,12 +2,12 @@
 #-*- coding:utf-8 -*-
 import shuangseqiu
 
-x=int(raw_input("双色球机选输入1；双色球手选请输入2:"))
+num=int(raw_input("双色球机选输入1；双色球手选请输入2:"))
 
 
-if x == 1:
+if num == 1:
 	shuangseqiu.jixuan_ssq()
-elif x == 2:
+elif num == 2:
     shuangseqiu.shouxuan_ssq()
 else:
     print u"输入错误"
--- shuangseqiu.py
+++ shuangseqiu.py
@@ -3,61 +3,54 @@
 print u"""#*******************************\n
 这是一个随机生成双色球号码的程序\n
 ********************************#
-
 """
 
 import random
 
 
-def jixuan_ssq():
 #定义自动选号函数
+def jixuan_ssq():
     i = 1
-    ssq_redball = []
-    ssq_blueball = []
+    ssq_hongqiu = []
+    ssq_lanqiu = []
     while i < 7 :
-        ssq_redball.append(random.randint(1,33))
-        #从1-33中随机生成一个数字
+	    #从1-33中随机生成一个数字 
+        ssq_hongqiu.append(random.randint(1,33))
         i += 1
-    if len(set(ssq_redball)) != 6:
-        #判断生成的6个数字中有没有重复的
-        ssq_redball.append(random.randint(1,33))
-        #如果有重复的继续生成至6个
+		#判断生成的6个数字中有没有重复的,如果有重复的继续生成至6个
+    if len(set(ssq_hongqiu)) != 6:
+        ssq_hongqiu.append(random.randint(1,33))
     else :
         pass
-    ssq_blueball.append(random.randint(1,14))
-    #从1-14中生成一个数字
-    print "随机生成一注双色球号码为:",sorted(set(ssq_redball)),ssq_blueball
-    #打印一组双色球（红球+篮球）号码
+	#从1-14中生成一个数字
+    ssq_lanqiu.append(random.randint(1,14))
+	#打印一组双色球（红球+篮球）号码
+    print u"随机生成一注双色球号码为:",sorted(set(ssq_hongqiu)),ssq_lanqiu
 
-def shouxuan_ssq():
+
+
+
 #定义手动选号函数
+def shouxuan_ssq():
     i = 1
-    ssq_redball = []
-    ssq_sdsr_blueball = []
+    ssq_hongqiu = []
+    ssq_lanqiu = None
     while i < 7:
-        ssq_sdsr_redball = int(raw_input("请输入选择[1..33]之间红球号码："))
-        #手动输入一个红球号码
-        i += 1
-        if  ssq_sdsr_redball <= 0 or ssq_sdsr_redball > 33:
-        #判断该红球号码是否<=0或者大于33
-            ssq_sdsr_redball = int(raw_input("请重新输入[1..33]之间红球号码："))
-        #输入的红球号码不在范围内，就重新输入
-        else:
-            pass
-        ssq_redball.append(ssq_sdsr_redball)
-    while len(set(ssq_redball)) != 6:
-        ssq_sdsr_redball = int(raw_input("请输入选择[1..33]之间红球号码："))
-        #判断输入的红球是否有重复的，有重复的需要重新输入
-        ssq_redball.append(ssq_sdsr_redball)
-    else :
-        pass
-    ssq_sdsr_blueball = int(raw_input("请输入选择[1..14]之间蓝球号码："))
-    #输入一个1-14之间的篮球号码
-    if ssq_sdsr_blueball <= 0 or ssq_sdsr_redball > 14:
-    #判断篮球号码是否在1-14之间
-        ssq_sdsr_blueball = int(raw_input("请输入选择[1..14]之间红球号码："))
-        #不在范围之内，需要重新输入
-    else:
-        pass
-    print "手动选号结果为:",sorted(set(ssq_redball)),ssq_sdsr_blueball
+	    #手动输入一个红球号码
+        ssq_sdsr_hongqiu = int(raw_input("请输入选择[1..33]之间红球号码："))
+		#判断输入的红球号码是否在范围（1-34）之内
+        if ssq_sdsr_hongqiu in [x for x in range(1,34)]:
+            ssq_hongqiu.append(ssq_sdsr_hongqiu)
+            i += 1
+			#判断输入的6个数字是否有重复的
+            if len(set(ssq_hongqiu)) == 6:
+                break
+    while True:
+        #手动输入一个篮球号码
+        ssq_lanqiu = int(raw_input("请输入选择[1..16]之间蓝球号码："))
+	    #判断输入的篮球号码是否在范围（1-16）之内
+        if ssq_lanqiu in [x for x in range(1,17)]:
+            break
     #输出一组手动选号双色球（红球+篮球）号码   
+    print u"手动选号结果为:",sorted(set(ssq_hongqiu)),ssq_lanqiu
+
